1e1e5aa9e3f3e7012ebe45c18b3bebadc59721be
Marcelo Pereira <marcelo96jr@gmail.com>
2025-08-28 13:18:55 -0300

    chore:  add validations in input single file component

--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -37,11 +37,15 @@ interface InputSingleFileProps
   extends VariantProps<typeof inputSingleFileVariants>,
     Omit<React.ComponentProps<'input'>, 'size'> {
   form: any;
+  allowedExtensions: string[];
+  maxFileSizeInMB: number;
   error?: React.ReactNode;
 }
 
 export default function InputSingleFile({
   form,
+  allowedExtensions,
+  maxFileSizeInMB,
   size,
   error,
   ...props
@@ -51,10 +55,29 @@ export default function InputSingleFile({
   const formFile: File = React.useMemo(() => {
     return formValues[name]?.[0];
   }, [formValues, name]);
+  const { fileExtension, fileSize } = React.useMemo(
+    () => ({
+      fileExtension: formFile?.name?.split('.')?.pop()?.toLowerCase() || '',
+      fileSize: formFile?.size || 0,
+    }),
+    [formFile]
+  );
+
+  function isValidExtension() {
+    return allowedExtensions.includes(fileExtension);
+  }
+
+  function isValidSize() {
+    return fileSize <= maxFileSizeInMB * 1024 * 1024;
+  }
+
+  function isValidFile() {
+    return isValidExtension() && isValidSize();
+  }
 
   return (
     <div className="w-full relative group cursor-pointer">
-      {!formFile ? (
+      {!formFile || !isValidFile() ? (
         <>
           <div>
             <input
@@ -76,11 +99,23 @@ export default function InputSingleFile({
             </div>
           </div>
 
-          {error && (
-            <Text variant="label-small" className="text-accent-red">
-              Error on upload
-            </Text>
-          )}
+          <div className="flex flex-col gap-1 mt-1">
+            {formFile && !isValidExtension() && (
+              <Text variant="label-small" className="text-accent-red">
+                File extension is not allowed
+              </Text>
+            )}
+            {formFile && !isValidSize() && (
+              <Text variant="label-small" className="text-accent-red">
+                Size of the file is too large
+              </Text>
+            )}
+            {error && (
+              <Text variant="label-small" className="text-accent-red">
+                {error}
+              </Text>
+            )}
+          </div>
         </>
       ) : (
         <div className="flex gap-3 items-center border border-solid border-border-primary mt-5 p-3 rounded">
