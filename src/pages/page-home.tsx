44f32cce5ba993be9ff4c547160a811c43748e6a
Marcelo Pereira <marcelo96jr@gmail.com>
2025-09-02 14:04:58 -0300

    chore: add routes config and pages

---
+++ src/pages/page-home.tsx
@@ -0,0 +1,5 @@
+import Text from '../components/text';
+
+export default function PageHome() {
+  return <Text variant="heading-large">Home</Text>;
+}
---
+++ src/pages/page-photo-details.tsx
@@ -0,0 +1,5 @@
+import Text from '../components/text';
+
+export default function PagePhotoDetails() {
+  return <Text variant="heading-large">Photo Details</Text>;
+}
