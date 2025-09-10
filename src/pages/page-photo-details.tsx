377c2fcfc83f193f54fc0eabf72eb10d36ad50f3
Marcelo Pereira <marcelo96jr@gmail.com>
2025-09-10 17:52:08 -0300

    chore: add photo details header and photos navigator

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,5 +1,35 @@
+import { useParams } from 'react-router';
+import Container from '../components/container';
 import Text from '../components/text';
+import type { Photo } from '../contexts/photos/models/photo';
+import Skeleton from '../components/skeleton';
+import PhotosNavigator from '../contexts/photos/components/photos-navigator';
 
 export default function PagePhotoDetails() {
-  return <Text variant="heading-large">Photo Details</Text>;
+  const { id } = useParams();
+  const isLoadingPhoto = false;
+  const photo = {
+    id: '1',
+    title: 'Sample Photo',
+    imageId: 'portrait-tower.png',
+    albums: [
+      { id: '1', title: 'Sample Album 1' },
+      { id: '2', title: 'Sample Album 2' },
+      { id: '3', title: 'Sample Album 3' },
+    ],
+  } as Photo;
+
+  return (
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large">{photo.title}</Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator loading={isLoadingPhoto} />
+      </header>
+    </Container>
+  );
 }
