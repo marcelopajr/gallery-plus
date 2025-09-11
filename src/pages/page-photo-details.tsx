1f59551fa7a2dcfbe79cb1e58f14490a90ee4390
Marcelo Pereira <marcelo96jr@gmail.com>
2025-09-11 10:53:14 -0300

    chore: add albums list selectable component

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -6,6 +6,7 @@ import Skeleton from '../components/skeleton';
 import PhotosNavigator from '../contexts/photos/components/photos-navigator';
 import ImagePreview from '../components/image-preview';
 import Button from '../components/button';
+import AlbumsListSelectable from '../contexts/albums/components/albums-list-selectable';
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
@@ -25,7 +26,9 @@ export default function PagePhotoDetails() {
     <Container>
       <header className="flex items-center justify-between gap-8 mb-8">
         {!isLoadingPhoto ? (
-          <Text variant="heading-large">{photo.title}</Text>
+          <Text as="h2" variant="heading-large">
+            {photo.title}
+          </Text>
         ) : (
           <Skeleton className="w-48 h-8" />
         )}
@@ -33,7 +36,7 @@ export default function PagePhotoDetails() {
         <PhotosNavigator loading={isLoadingPhoto} />
       </header>
 
-      <div className="grid grid-cols-[21rem] gap-24">
+      <div className="grid grid-cols-[21rem_1fr] gap-24">
         <div className="space-y-3">
           {!isLoadingPhoto ? (
             <ImagePreview
@@ -50,6 +53,22 @@ export default function PagePhotoDetails() {
           ) : (
             <Skeleton className="w-20 h-10" />
           )}
+        </div>
+
+        <div className="py-3">
+          <Text variant="heading-medium" className="mb-6" as="h3">
+            Albums
+          </Text>
+
+          <AlbumsListSelectable
+            albums={[
+              { id: '1', title: 'Sample Album 1' },
+              { id: '2', title: 'Sample Album 2' },
+              { id: '3', title: 'Sample Album 3' },
+            ]}
+            photo={photo}
+            loading={isLoadingPhoto}
+          />
         </div>
       </div>
     </Container>
