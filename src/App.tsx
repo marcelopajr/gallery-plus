6de1a82f63100ea02c4dcd69cbfac2cdaccffead
Marcelo Pereira <marcelo96jr@gmail.com>
2025-09-23 11:27:34 -0300

    chore: add react query client and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LayoutMain from './pages/layout-main';
 import PageHome from './pages/page-home';
 import PagePhotoDetails from './pages/page-photo-details';
 import PageComponents from './pages/page-components';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<LayoutMain />}>
-          <Route index element={<PageHome />} />
-          <Route path="/photos/:id" element={<PagePhotoDetails />} />
-          <Route path="/components" element={<PageComponents />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LayoutMain />}>
+            <Route index element={<PageHome />} />
+            <Route path="/photos/:id" element={<PagePhotoDetails />} />
+            <Route path="/components" element={<PageComponents />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
