bdd3ce1dcd6f7d0a56f3f0048ea05997387b8183
Marcelo Pereira <marcelo96jr@gmail.com>
2025-10-01 17:54:22 -0300

    feat: add filter by query

--- src/components/photos-search.tsx
+++ src/components/photos-search.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import InputText from './input-text';
 import SearchIcon from '../assets/icons/search.svg?react';
 import { debounce } from '../helpers/utils';
+import usePhotos from '../contexts/photos/hooks/use-photos';
 
 export default function PhotosSearch() {
   const [inputValue, setInputValue] = React.useState('');
+  const { filters } = usePhotos();
 
   const debounceSetValue = React.useCallback(
     debounce((value: string) => {
-      console.log('Searching for:', value);
-    }, 1000),
-    []
+      filters.setQ(value);
+    }, 200),
+    [filters.setQ]
   );
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
