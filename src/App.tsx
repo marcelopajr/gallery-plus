7b507b5b29d7f4e1712186664caeeac1aa90e546
Marcelo Pereira <marcelo96jr@gmail.com>
2025-10-01 16:54:04 -0300

    chore: add nuqs into app

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import LayoutMain from './pages/layout-main';
 import PageHome from './pages/page-home';
 import PagePhotoDetails from './pages/page-photo-details';
 import PageComponents from './pages/page-components';
+import { NuqsAdapter } from 'nuqs/adapters/react-router/v7';
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<LayoutMain />}>
-            <Route index element={<PageHome />} />
-            <Route path="/photos/:id" element={<PagePhotoDetails />} />
-            <Route path="/components" element={<PageComponents />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <NuqsAdapter>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<LayoutMain />}>
+              <Route index element={<PageHome />} />
+              <Route path="/photos/:id" element={<PagePhotoDetails />} />
+              <Route path="/components" element={<PageComponents />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </NuqsAdapter>
     </QueryClientProvider>
   );
 }
